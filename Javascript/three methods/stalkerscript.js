303698c4b322755cdf135a47245eb829d1ea8521
maolintuntun <39028815+maolintuntun@users.noreply.github.com>
2019-04-22 00:51:40 -0400

    Update stalkerscript.js

--- Javascript/three methods/stalkerscript.js
+++ Javascript/three methods/stalkerscript.js
@@ -1,5 +1,6 @@
-var userFirstname = prompt("What is your first name?");
+var userFirstname = prompt("What is your first name?"); /*先把输入值存在变量中*/
 var userLastname = prompt("What is your last name?");
 var userAge = prompt("How old are you?");
-console.log(userFirstname + " " + userLastname);
+console.log(userFirstname + " " + userLastname);  /* !!!不要写成“userFirstname” 不要加双引号，他已经是一个string*/
+ 
 console.log("I'm " + userAge + " years old");
